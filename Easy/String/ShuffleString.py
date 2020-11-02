c10ed887ea94f03f48aba04a7d54d2485f9dfc7f
Nikhita Karnati <32843455+nikk9999@users.noreply.github.com>
2020-11-01 22:23:28 -0800

    Update ShuffleString.py

--- Easy/String/ShuffleString.py
+++ Easy/String/ShuffleString.py
@@ -1,5 +1,14 @@
 # https://leetcode.com/problems/shuffle-string/
+# Best: https://leetcode.com/problems/shuffle-string/discuss/755923/Used-Cyclic-Sort-with-O(1)-SPACE-and-O(N)-Time-complexity
+
 # Sol 1
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res = ['']*len(indices)
+        for index, char in enumerate(s):
+            res[indices[index]] = char
+        return "".join(res)
+# Sol 2
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         visited = [False]*len(indices)
@@ -15,7 +24,7 @@ class Solution:
                 t = t1
         return "".join(strList)
         
-# Sol 2
+# Sol 3
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         resultList = ['']*len(indices)
